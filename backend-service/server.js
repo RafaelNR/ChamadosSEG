caa721ae1ee710206a8903b02d413345a0c8b53e
rafaelnr <rafaelnetto_@hotmail.com>
2021-01-15 15:00:36 -0300

    Ecosystem

--- backend-service/server.js
+++ backend-service/server.js
@@ -4,7 +4,14 @@ const knex = require("./src/database");
 
 App.listen(process.env.BACK_PORT, (err) => {
 	if (err) console.log("> [Server] - Down -> , erro: " + err);
-	console.log("> [Server] - Running...");
+	console.log("> [BACKEND SERVICE] - Running...");
+	console.log("> [BACKEND SERVICE SERVICE] - NODE: ", process.env.NODE_ENV);
+	console.log("> [BACKEND SERVICE] - Nome Service: ", process.env.APP_NAME);
+	console.log("> [BACKEND SERVICE] - Connecion IP: ", process.env.IP);
+	console.log("> [BACKEND SERVICE] - DB HOST: ", process.env.DB_HOST);
+	console.log("> [BACKEND SERVICE] - DB NAME: ", process.env.DB_NAME);
+	console.log("> [BACKEND SERVICE] - DB USER: ", process.env.DB_USER);
+	console.log("> [BACKEND SERVICE] - DB PASSWD: ", process.env.DB_PASSWD);
 });
 
 // Quando finalizar o express, finaliza todas os processos e fecha o banco de dados.
--- backend/index.js
+++ backend/index.js
@@ -7,10 +7,13 @@ const knex = require('./src/database');
 App.listen(process.env.BACK_PORT, (err) => {
 	if (err) return console.log("> [BACKEND] - Down -> , erro: " + err);
 	console.log("> [BACKEND] - Running...");
+	console.log("> [BACKEND] - NODE: ", process.env.NODE_ENV);
 	console.log("> [BACKEND] - Nome Service: ", process.env.APP_NAME);
 	console.log("> [BACKEND] - Connecion IP: ", process.env.IP);
-	console.log("> [BACKEND] - BackEnd Port: ", process.env.BACK_PORT);
-	console.log("> [BACKEND] - NODE: ", process.env.NODE_ENV);
+	console.log("> [BACKEND] - DB HOST: ", process.env.DB_HOST);
+	console.log("> [BACKEND] - DB NAME: ", process.env.DB_NAME);
+	console.log("> [BACKEND] - DB USER: ", process.env.DB_USER);
+	console.log("> [BACKEND] - DB PASSWD: ", process.env.DB_PASSWD);
 });
 
 // Quando finalizar o express, finaliza todas os processos e fecha o banco de dados.
