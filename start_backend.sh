0567508a71cfcb971664430bf9817df29c708153
rafaelnr <rafaelnetto_@hotmail.com>
2021-07-15 16:19:54 -0300

    Melhoras nos scripts de gestão.

--- start_backend.sh
+++ start_backend.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-
 echo "Iniciando o start do PM2 - Backend"
 cd /usr/share/ostecnico/backend/
 mv .env.production .env
 pm2 start ecosystem.config.js --env prod
 echo "BackEnd iniciado..."
-
 
 echo "Iniciando o start do PM2 - Backend-service"
 cd /usr/share/ostecnico/backend-service/
--- update.sh
+++ update.sh
@@ -1,21 +1,48 @@
 #!/bin/bash
 
-git checkout -- .
+declare -r DIR_OS='/usr/share/ostecnico'
+declare -r TYPE=$1
 
-echo 'Iniciando a atualização.'
-cd /usr/share/ostecnico/
-git pull
 
-echo 'Atualizando Backend.'
-cd /usr/share/ostecnico/backend/
-mv .env.production .env
-cd /usr/share/ostecnico/backend-service/
-mv .env.production .env
+if [ $TYPE = 'frontend' ]; then
+  echo 'Atualizando o FrontEnd.'
+    cd "$DIR_OS"/frontend
+    yarn build-no-mapper
+    yes | cp -R build/* /usr/share/nginx/html/
+  echo 'Finalizado.'
+else
+  echo 'Backup do banco dados'
+  mysqldump --single-transaction -u root -pSeg@2021 ostecnicos > /tmp/ostecnicos.sql
 
-echo 'Restart do backend.'
-pm2 restart all
+  echo 'Iniciando a atualização.'
+    cd $DIR_OS
+    git checkout -- .
+    git pull
 
-echo 'Atualizando o FrontEnd.'
-cd /usr/share/ostecnico/frontend
-yarn build-no-mapper
-yes | cp -R build/* /usr/share/nginx/html/
+  echo 'Atualizando Backend.'
+    cd "$DIR_OS"/backend/
+    yarn -i
+    echo 'Atualização do banco de dados caso tenha.'
+    yarn knex migrate:latest
+    echo 'Move ENV'
+    mv .env.production .env
+  echo 'Finalizado.'
+
+  echo 'Atualizando Backend-Service.'
+    cd "$DIR_OS"/backend-service/
+    yarn -i
+    echo 'Atualização do banco de dados caso tenha.'
+    yarn knex migrate:latest
+    echo 'Move ENV'
+    mv .env.production .env
+  echo 'Finalizado.'
+
+  echo 'Restart do backend.'
+    pm2 restart all
+
+  echo 'Atualizando o FrontEnd.'
+    cd "$DIR_OS"/frontend
+    yarn build-no-mapper
+    yes | cp -R build/* /usr/share/nginx/html/
+  echo 'Finalizado.'  
+fi
